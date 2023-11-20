ee4c4ed6778b7d3325aa83e3164b9acffe8cf9a0
hmuri <113418916+hmuri@users.noreply.github.com>
2023-11-21 02:51:30 +0900

    feat: 기초 환경 세팅 성공

---
+++ src/main/java/com/example/LastSpring/LastSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.LastSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LastSpring/LastSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LastSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
